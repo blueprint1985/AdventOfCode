3d41348fdf2969b3a783387b7aae1b7a2e035b4f
blueprint1985 <martinbjorling@gmail.com>
2017-12-05 20:42:20 +0100

    Remove function to make solution more cleanly mathematical

--- 2017/Day 03/day03_1.php
+++ 2017/Day 03/day03_1.php
@@ -1,25 +1,5 @@
 <?php
 
-/**
- * shortestDistToMid
- * 
- * Calculate the shortest distance to a midpoint.
- *
- * @param int $input The input that we need to know the Manhattan distance from
- * @param int[] $midpoints Array containing the four midpoints on the edge of the square
- * @param int $square_size The width and height of the square
- * @return int The shortest distance from the input to a midpoint
- */
-function shortestDistToMid(int $input, array $midpoints, int $square_size) : int {
-    $distance = $square_size;
-
-    foreach ($midpoints as $value) {
-        $distance = min($distance, abs($input - $value));
-    }
-
-    return $distance;
-}
-
 // READ INPUT
 
 $myfile = fopen("day03_input.txt", "r") or die("Unable to open file!");
@@ -54,12 +34,18 @@ $square_mid_left = $square_mid_bottom - ($square_size - 1);
 $square_mid_top = $square_mid_left - ($square_size - 1);
 $square_mid_right = $square_mid_top - ($square_size - 1);
 
+// Calculate the distance from the input to each endpoint. Use absolute value
+// so that all distances are positive.
+$mid_bottom_dist = abs($input - $square_mid_bottom);
+$mid_left_dist = abs($input - $square_mid_left);
+$mid_top_dist = abs($input - $square_mid_top);
+$mid_right_dist = abs($input - $square_mid_right);
+
 // When we know the midpoints we need to calculate shortest distance from a
 // midpoint to our value. After that we add the distance from that midpoint to
 // the center. That gives is the Manhattan distance.
-$midpoints = array($square_mid_bottom, $square_mid_left, $square_mid_top, $square_mid_right);
-$midpoint_dist = shortestDistToMid($input, $midpoints, $square_size);
-$manhattan_dist = $midpoint_dist + (floor($square_size / 2));
+$min_midpoint_dist = min($mid_bottom_dist, $mid_left_dist, $mid_top_dist, $mid_right_dist);
+$manhattan_dist = $min_midpoint_dist + (floor($square_size / 2));
 
 // Stop timing
 $end_time = microtime(true);
