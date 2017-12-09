dcbafba8f03d57d716490f1849703fd6fdff48a5
blueprint1985 <martinbjorling@gmail.com>
2017-12-09 14:27:47 +0100

    AoC 2017 day 9

---
+++ 2017/Day09/PartOne.php
@@ -0,0 +1,72 @@
+<?php
+
+class PartOne extends Base {
+    private $stream;
+
+    function __construct($input) {
+        $this->stream = $input;
+    }
+
+    //private function -+
+
+    public function solve() {
+        // Fetch programs variable to save runtime
+        $stream = $this->stream;
+
+        // Initiate variables
+        $sum = 0;
+        $depth_counter = 0;
+        $i = 0;
+
+        // Loop through entire stream
+        while ($i < strlen($stream)) {
+            // We found a valid group start, increase depth counter and add to
+            // total sum, or a valid group ending, decrease depth counter
+            if ($stream[$i] === "{") {
+                $depth_counter++;
+                $sum += $depth_counter;
+            } else if ($stream[$i] === "}") {
+                $depth_counter--;
+            }
+
+            $i++;
+
+            // If current char is start if garbage, find garbage end
+            if ($stream[$i] === "<") {
+                $i++;
+                $cancel_next = false;
+                $garbage_ended = false;
+
+                while (!$garbage_ended) {
+                    // If this char was cancelled
+                    if ($cancel_next) {
+                        $cancel_next = false;
+                        $i++;
+                        continue;
+                    }
+
+                    // If this char is "!", cancel next
+                    if ($stream[$i] === "!") {
+                        $cancel_next = true;
+                        $i++;
+                        continue;
+                    }
+
+                    // If this char is ">" and not cancelled, we found the end
+                    if ($stream[$i] === ">") {
+                        $garbage_ended = true;
+                        $i++;
+                        continue;
+                    }
+
+                    // Otherwise, increase  i
+                    $i++;
+                }
+            }
+        }
+
+        return $sum;
+    }
+}
+
+?>
---
+++ 2017/Day09/PartTwo.php
@@ -0,0 +1,61 @@
+<?php
+
+class PartTwo extends Base {
+    private $stream;
+
+    function __construct($input) {
+        $this->stream = $input;
+    }
+
+    public function solve() {
+        // Fetch programs variable to save runtime
+        $stream = $this->stream;
+
+        $garbage = 0;
+        $i = 0;
+
+        // Loop through entire stream
+        while ($i < strlen($stream)) {
+            $i++;
+
+            // If current char is start if garbage, find garbage end
+            if ($stream[$i] === "<") {
+                $i++;
+                $cancel_next = false;
+                $garbage_ended = false;
+
+                // Search until we find end of garbage
+                while (!$garbage_ended) {
+                    // If this char was cancelled
+                    if ($cancel_next) {
+                        $cancel_next = false;
+                        $i++;
+                        continue;
+                    }
+
+                    // If this char is "!", cancel next
+                    if ($stream[$i] === "!") {
+                        $cancel_next = true;
+                        $i++;
+                        continue;
+                    }
+
+                    // If this char is ">" and not cancelled, we found the end
+                    if ($stream[$i] === ">") {
+                        $garbage_ended = true;
+                        $i++;
+                        continue;
+                    }
+
+                    // Otherwise, increase garbage amount and i
+                    $garbage++;
+                    $i++;
+                }
+            }
+        }
+
+        return $garbage;
+    }
+}
+
+?>
