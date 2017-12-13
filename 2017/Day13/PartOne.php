6eb9c1533e9c226a95f2e93a47c50f9f638e7643
blueprint1985 <martinbjorling@gmail.com>
2017-12-13 10:05:16 +0100

    AoC 2017 day 13

---
+++ 2017/Day13/PartOne.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Class for problem part one
+ *
+ * @author Martin Björling <martinbjorling@gmail.com>
+ * @license MIT
+ * @copyright Martin Björling 2015 - 2017
+ */
+class PartOne extends Base {
+    private $layers;
+
+    function __construct($input) {
+        $this->layers = $input;
+    }
+
+    /**
+     * solve
+     *
+     * Solves the problem based on the input
+     *
+     * @return string The solution for the problem
+     */
+    public function solve() : string {
+        // Fetch layers variable to save runtime
+        // Make better array for each layer, a layer will look loke this:
+        // 0 => ["range" => 3, "roundtime" => 4]
+        $layers = array();
+        foreach ($this->layers as $layer) {
+            list($layer, $range) = explode(": ", $layer);
+            $roundtime = (intval($range) - 1) * 2;
+            $layers[intval($layer)] = array("range" => intval($range), "roundtime" => $roundtime);
+        }
+
+        // Initiate variables
+        $last_layer_index = max(array_keys($layers));
+        $severity = 0;
+
+        // Step through firewall
+        for ($i=0; $i < $last_layer_index + 1; $i++) {
+            // There is no layer this step
+            if (empty($layers[$i])) {
+                continue;
+            }
+
+            // Layer i scanner position at step i
+            $pos_i = $i % $layers[$i]["roundtime"];
+
+            // If layer i scanner is at position 0 at step i, add severity
+            if ($pos_i === 0) {
+                $severity += ($i * $layers[$i]["range"]);
+            }
+        }
+
+        return strval($severity);
+    }
+}
+
+?>
---
+++ 2017/Day13/PartTwo.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Class for problem part two
+ *
+ * @author Martin Björling <martinbjorling@gmail.com>
+ * @license MIT
+ * @copyright Martin Björling 2015 - 2017
+ */
+class PartTwo extends Base {
+    private $layers;
+
+    function __construct($input) {
+        $this->layers = $input;
+    }
+
+    /**
+     * solve
+     *
+     * Solves the problem based on the input
+     *
+     * @return string The solution for the problem
+     */
+    public function solve() : string {
+        // Fetch layers variable to save runtime
+        // Make better array for each layer, a layer will look loke this:
+        // 0 => ["range" => 3, "roundtime" => 4]
+        $layers = array();
+        foreach ($this->layers as $layer) {
+            list($layer, $range) = explode(": ", $layer);
+            $roundtime = (intval($range) - 1) * 2;
+            $layers[intval($layer)] = array("range" => intval($range), "roundtime" => $roundtime);
+        }
+
+        // Initiate variables
+        $last_layer_index = max(array_keys($layers));
+        $severity = 1;
+        $start_time = -1;
+
+        // Loop while we have severity (we have severity if we hit the scanner)
+        while ($severity !== 0) {
+            // Reset severity and increase start time
+            $severity = 0;
+            $start_time++;
+
+            for ($i=0; $i < $last_layer_index + 1; $i++) {
+                // There is no layer this step
+                if (empty($layers[$i])) {
+                    continue;
+                }
+
+                // Layer i scanner position at step i with start time added
+                $pos_i = ($i + $start_time) % $layers[$i]["roundtime"];
+
+                // If layer i scanner is at position 0 at step i, add severity
+                if ($pos_i === 0) {
+                    $severity += ($i * $layers[$i]["range"]);
+
+                    // Special case for layer 0 because 0 * i is 0 even when
+                    // scanner is at position 0
+                    if ($i === 0) {
+                        $severity++;
+                    }
+                }
+            }
+        }
+
+        return strval($start_time);
+    }
+}
+
+?>
